e727be08161968f93c2580bdf24fd9bc10e1fa1e
Ilaria44 <palla.rotolante@gmail.com>
2024-08-20 15:51:50 +0200

    pass test 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(stringToBeRepeated, numOfRepetitions) {
+  let finalString = "";
 
+  if(numOfRepetitions >= 0) {
+
+    for (let i = 0; i < numOfRepetitions; i++) {
+      finalString += stringToBeRepeated;
+    };
+
+  } else {
+    finalString = "ERROR";
+  }
+
+  return finalString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+  let textArray = text.split("");
+  textArray.reverse();
+
+  let newText = textArray.join("");
+
+  return newText;
+  
 };
 
 // Do not edit below this line
