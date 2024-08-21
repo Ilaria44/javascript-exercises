9630488f9fcea3ad47f9eaa245440796033566ca
Ilaria44 <palla.rotolante@gmail.com>
2024-08-21 19:16:17 +0200

    resolve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+
+  for (item of itemsToRemove) {
+    //let pos = array.indexOf(item);
+    //array.splice(pos, 1);
+
+    let i = 0;
+    while (i < array.length) {
+      if (item === array[i]) {
+        array.splice(i, 1);
+      } else {
+        i++;
+      }
+
+    }
+  }
+ 
+
+  return array;
+
 };
 
 // Do not edit below this line
