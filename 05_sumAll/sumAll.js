fb544a9e1a9337cf89af06358ab9f82003bfb3a1
Ilaria44 <palla.rotolante@gmail.com>
2024-08-22 09:37:27 +0200

    resolve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+  let sum = 0;
+  
+  if (typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0 || Math.floor(num1) != num1 || Math.floor(num2) != num2) {
+    return "ERROR";
+  }
+
+  if (num1 <= num2) {
+    for (i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  } else {
+    for (i = num2; i <= num1; i++) {
+      sum += i;
+    }
+  }
+
+
+  return sum;
+
 
 };
 
