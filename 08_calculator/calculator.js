8bde1801166d54c3360eaae9c62c25bdf9edf424
Ilaria44 <palla.rotolante@gmail.com>
2024-08-23 17:02:03 +0200

    resolve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let result = num1 + num2;
+
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let result;
+  if(num1 <= num2) {
+    result = num2 - num1;
+  } else {
+    result = num1 - num2;
+  }
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let result = 0;
+	for (num of numArray) {
+    result += num;
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let result = numArray[0];
 
+  for (i = 1; i < numArray.length; i++) {
+    result *= numArray[i];
+  }
+
+  return result;
 };
 
-const power = function() {
+const power = function(num1, num2) {
+  let result = num1 ** num2;
 	
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let result = num;
+
+  for (i = 1; i < num; i++) {
+    result *= i;
+  }
+  
+  return result;	
 };
 
 // Do not edit below this line
