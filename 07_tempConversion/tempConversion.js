df9ccca6f3c8467b1524121cb8a103772b627327
Ilaria44 <palla.rotolante@gmail.com>
2024-08-23 11:00:18 +0200

    resolve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegrees) {
+  let celsiusDegrees = (fahrenheitDegrees - 32) * (5 / 9);
+  celsiusDegrees = Number(celsiusDegrees.toFixed(1));
+  return celsiusDegrees;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  let fahrenheitDegrees = celsiusDegrees * (9 / 5) + 32;
+  fahrenheitDegrees = Number(fahrenheitDegrees.toFixed(1));
+  return fahrenheitDegrees;
 };
 
 // Do not edit below this line
