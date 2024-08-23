3bdd7c5acaea35f0895343a059acfca4005492fd
Ilaria44 <palla.rotolante@gmail.com>
2024-08-23 17:53:41 +0200

    resolve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+  let newString = string.toLowerCase().replaceAll(/[^a-z^0-9]/g, "");
+
+  let reverseString = newString.split("").reverse().join("");
+
+  if (newString === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
+  
+  
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
