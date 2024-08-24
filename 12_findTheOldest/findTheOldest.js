818c3814b474e331326177f365a095c46ea3fdc9
Ilaria44 <palla.rotolante@gmail.com>
2024-08-24 15:43:12 +0200

    resolve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+  
+  const thisYear = new Date().getFullYear();
+
+  let age = peopleArray.map(person => {
+    if(person.yearOfDeath) {
+      return person.yearOfDeath - person.yearOfBirth;
+    } else {
+      return thisYear - person.yearOfBirth;
+    }
+    
+    
+  })
+
+
+  let oldestAge = 0;
+
+  for (const each of age) {
+    if(each > oldestAge) {
+      oldestAge = each;
+    }
+  }
+
+  return peopleArray[age.indexOf(oldestAge)];
 
 };
 
