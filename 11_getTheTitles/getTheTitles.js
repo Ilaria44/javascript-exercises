cd39b82a1459683d011116cfb45a97f6ead98822
Ilaria44 <palla.rotolante@gmail.com>
2024-08-24 12:45:44 +0200

    resolve exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArray) {
+  let titlesArray = [];
 
+  for (book in booksArray) {
+    titlesArray.push(booksArray[book].title);
+  }
+  return titlesArray;
 };
 
 // Do not edit below this line
