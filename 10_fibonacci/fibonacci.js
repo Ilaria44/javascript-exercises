cd5385b199ed946d1f6a5292bc38c68b6dc09a8e
Ilaria44 <palla.rotolante@gmail.com>
2024-08-24 12:34:51 +0200

    resolve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  
+  if(typeof(num) != "number") {
+    num = Number(num);
+  }
 
+  if(num < 0) return "OOPS";
+  
+
+
+  let fibonacciNum = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    fibonacciNum[i] = fibonacciNum[i-1] + fibonacciNum[i-2];
+
+  }
+
+
+  return fibonacciNum [num];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
